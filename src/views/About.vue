270c8250ddd0cbd0c10b5b082a3f138cd0bca5ba
Victor-Aremu <victor.aremu@amalitech.org>
2021-07-04 19:51:17 +0000

    tasks added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus
+      facilis dolor quod sunt tenetur laborum. Odit asperiores, temporibus ea
+      animi sed omnis odio consequuntur exercitationem fugit autem, eius vitae.
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <v-list class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+
+      <v-list-item 
+      @click="doneTask(task.id)">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox :input-value="task.done"
+            color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ task.title }}</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+      <v-divider class="mx-4"></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake Up",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Buy Bananas",
+          done: false,
+        },
+        {
+          id: 3,
+          title: "Eat Bananas",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    doneTask(id){
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    }
+  },
+};
+</script>
