2fc7ba7abe4c553b29bc91d3fb6865d43f54a9ab
Victor-Aremu <victor.aremu@amalitech.org>
2021-07-04 20:18:35 +0000

    about page updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,8 @@
   <div class="about pa-6">
     <h1>About Vuetify Todo</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, natus
-      facilis dolor quod sunt tenetur laborum. Odit asperiores, temporibus ea
-      animi sed omnis odio consequuntur exercitationem fugit autem, eius vitae.
+      Created By Victor Aremu <br>
+      <span> 4th July 2021</span>
     </p>
   </div>
 </template>
